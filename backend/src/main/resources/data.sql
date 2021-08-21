b1c7efe70220c4ff8d79e6b7e1b000519ddb246e
Miguel Morais <miguelmoraisdev@gmail.com>
2021-08-20 21:27:44 -0300

    Lesson seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -28,3 +28,20 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 01 do Cap 01', 1, 1);
+INSERT INTO tb_content (text_Content, video_Uri, id) VALUES ('Material de apoio ABC', 'https://www.youtube.com/watch?v=os6hdZbCnpM&t=16s', 1);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 02 do Cap 01', 2, 1);
+INSERT INTO tb_content (text_Content, video_Uri, id) VALUES ('Material', 'https://www.youtube.com/watch?v=os6hdZbCnpM&t=16s', 2);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 03 do Cap 01', 3, 1);
+INSERT INTO tb_content (text_Content, video_Uri, id) VALUES ('Material', 'https://www.youtube.com/watch?v=os6hdZbCnpM&t=16s', 3);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Desafio 01 do Cap 01', 4, 1);
+INSERT INTO tb_task (description, question_Count, approval_Count, weight, due_Date,  id) VALUES ('Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-11-25T13:00:00Z',4);
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+
+
